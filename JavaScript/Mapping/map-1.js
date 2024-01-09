5866ea1af7503c7d5607231a6b8d85fa3ebaf8c9
Prasanga Pokharel <138413854+prasangapokharel@users.noreply.github.com>
2024-01-09 09:47:51 +0545

    Add files via upload

---
+++ JavaScript/Mapping/map-1.js
@@ -0,0 +1,8 @@
+let newarr = [1,2,3,4,5,6,7,8,9,10]
+
+let mapdata = newarr.map((item) => {
+    return item +2
+})
+
+console.log(mapdata, 'This is mapped result')
+console.log(newarr, 'This is mapped result')
